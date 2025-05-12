deb9cc1e4afe4f51efba02d7bad786ebbcd0058f
Hayshem <hayshem2009@gmail.com>
2025-05-12 11:32:16 +0200

    Last update

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from prophet.plot import plot_plotly
 from google.oauth2.service_account import Credentials
 import io
 import pandas as pd
+import plotly.graph_objects as go 
 
 
 # Get credentials from Streamlit secrets
