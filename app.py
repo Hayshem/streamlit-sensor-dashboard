a75dd38f8e7a9d173fa9f2ea3cdc52111aa82361
Hayshem <hayshem2009@gmail.com>
2025-05-20 11:00:25 +0200

    update

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ logo1 = Image.open("images.png")  # First logo path
 logo2 = Image.open("NODES_Logo.png")  # Second logo path
 
 #logo1 = logo1.resize((int(logo1.width * 0.5), int(logo1.height * 0.5)))  # Adjust scale as needed
-logo2 = logo2.resize((int(logo2.width * 0.8), int(logo2.height * 0.3)))
+logo2 = logo2.resize((int(logo2.width * 0.8), int(logo2.height * 0.4)))
 
 # A layout with two columns for logos
 col1, col2 = st.columns(2)
