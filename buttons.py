354cfe3ebd6596903c509d60bb1f61c83cb92a53
Hayshem <hayshem2009@gmail.com>
2025-06-13 11:10:55 +0200

    Switch from Drive CSV download to Sheets API reader

--- buttons.py
+++ buttons.py
@@ -3,10 +3,10 @@ import streamlit as st
 st.markdown("""
 <style>
 div.stButton > button {
-    height: 75px; /* Adjust height for larger buttons */
+    height: 85px; /* Adjust height for larger buttons */
     width: 150px;  /* Adjust width for square buttons */
     font-size: 20px; /* Adjust font size */
-    margin: 5px;  /* Add space between buttons */
+    margin: 8px;  /* Add space between buttons */
     color: white;  /* Button text color */
     border-radius: 15px; /* Rounded corners */
 }
